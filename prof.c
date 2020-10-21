3f7fec2915e32d9185a7eb0d2b162d6c63f6097e
ruhi0608 <73190762+ruhi0608@users.noreply.github.com>
2020-10-21 23:22:41 +0530

    Update prof.c

--- prof.c
+++ prof.c
@@ -1,1 +1,18 @@
+#include<stdio>
+
+{
+int i;
+
+printf("Enter a Number to know whether it is even or odd\n");
+scanf("%d",i);
+if(i/2==0)
+{
+printf("The number %d is Even",i);
+
+}
+else
+{
+printf("The number %d is Odd",i);
+}
+printf("\n");
 
